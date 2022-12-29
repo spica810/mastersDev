3a7f1b4206013018e9d3bacbc59251c660f900f9
spica810 <118879143+spica810@users.noreply.github.com>
2022-12-29 14:13:19 +0900

    Update Hello.java
    학습용

--- Hello.java
+++ Hello.java
@@ -2,6 +2,8 @@ public class Hello{
     public static void main(String [] args){
         System.out.println("Hello Github");
         
+        System.out.println("Test Text");
+        
         System.out.println("GiirrrMaster add line");
         
     }
